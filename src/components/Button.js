a9b4d563270c9c110dd08ea3102edaafc29c26af
fgfl <frederick.gfl@gmail.com>
2020-01-19 23:51:04 -0800

    Add default button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {action } from "@storybook/addon-actions/dist/preview";
 
 import "components/Button.scss";
 
+export const actionsData = {
+   onClick:  action('onClick'),
+};
+
 export default function Button(props) {
-   return <></>;
+   return <button>{props.children}</button>
 }
+//    props:{
+//       danger,
+//       confirm,
+//       disabled
+//    },
+//    onClick
+//    }) {
+//    return (
+//       <button className={`
+//          ${danger && ".button--danger"}
+//          ${confirm && ".button--confirm"}
+//          ${disabled && ".button--disabled"}
+//          `} onClick={() => onClick()}> 
+//          {props.children}
+//       </button>
+//    );
+// }
