72508847daa0c220be0f72e3bbd03dc39348af2a
fgfl <frederick.gfl@gmail.com>
2020-01-20 00:39:42 -0800

    Removed unnessecary code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,7 @@
 import React from "react";
-import {action } from "@storybook/addon-actions/dist/preview";
 import classNames from "classnames";
 
 import "components/Button.scss";
-
-export const actionsData = {
-   onClick:  action('button-clicked'),
-};
 
 export default function Button(props) {
    let buttonClass = classNames({
@@ -25,20 +20,3 @@ export default function Button(props) {
       </button>
    );
 }
-//    props:{
-//       danger,
-//       confirm,
-//       disabled
-//    },
-//    onClick
-//    }) {
-//    return (
-//       <button className={`
-//          ${danger && ".button--danger"}
-//          ${confirm && ".button--confirm"}
-//          ${disabled && ".button--disabled"}
-//          `} onClick={() => onClick()}> 
-//          {props.children}
-//       </button>
-//    );
-// }
