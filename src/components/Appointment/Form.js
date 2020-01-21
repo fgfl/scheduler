cd67c8909ddeb9faab414afac288ff022cea6899
fgfl <frederick.gfl@gmail.com>
2020-01-20 20:14:38 -0800

    Add reset functionality to cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import InterviewerList from '../InterviewerList';
 import Button from '../Button';
 
+
 /**
  * 
  * @param {{
@@ -16,6 +17,16 @@ import Button from '../Button';
 const Form = (props) => {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName('');
+    setInterviewer(null);
+  };
+
+  const cancel = (onCancel) => {
+    reset();
+    onCancel();
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -37,7 +48,7 @@ const Form = (props) => {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={(event) => cancel(props.onCancel)}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
