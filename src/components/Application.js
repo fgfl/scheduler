8d173bf54a57e34362c8b450a40e283caacc53d6
fgfl <frederick.gfl@gmail.com>
2020-01-20 20:59:40 -0800

    Load Appointments into the application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import "components/Application.scss";
 
 import DayList from 'components/DayList';
+import {Appointment} from 'components/Appointment/index'
 
 const days = [
   {
@@ -22,8 +23,63 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: '2pm',
+    interview: {
+      student: 'Joe',
+      interviewer: { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+    }
+  },
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Swanson',
+      interviewer: { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+    }
+  },
+  {
+    id: 5,
+    time: '6pm',
+    interview: {
+      student: 'Peter Griffin',
+      interviewer: { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+    }
+  },
+  {
+    id: 6,
+    time: 'last',
+  }
+];
+
+
 const Application = () => {
   const [day, setDay] = useState('Monday');
+  const appointmentItems = 
+    appointments.map((appointment) => (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    ));
 
   return (
     <main className="layout">
@@ -52,7 +108,7 @@ const Application = () => {
         }
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentItems}
       </section>
     </main>
   );
