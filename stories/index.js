573de8d5a8e77f56e773e979abe981e8ba1a5994
fgfl <frederick.gfl@gmail.com>
2020-01-20 19:15:39 -0800

    Add storybook components to Appointment

--- stories/index.js
+++ stories/index.js
@@ -163,8 +163,15 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-  .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () => (
+  .add("Status-Saving", () => <Status message="Saving" />)
+  .add("Status-Deleteing", () => <Status message="Deleting" />)
+  .add("Error-Saving", () => (
+    <Error
+      message="Could not save appointment"
+      onClose={action("onClose")}
+    />
+  ))
+  .add("Error-Deleting", () => (
     <Error
       message="Could not delete appointment"
       onClose={action("onClose")}
