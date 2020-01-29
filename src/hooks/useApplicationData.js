3e68b0612662978acf7565e4453257c650f24170
fgfl <frederick.gfl@gmail.com>
2020-01-28 16:30:45 -0800

    Moved reducer to own file for unit testing default case

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,66 +1,16 @@
 import { useReducer, useEffect } from 'react';
 import axios from 'axios';
-
-const SET_DAY = 'SET_DAY';
-const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
-const SET_INTERVIEW = 'SET_INTERVIEW';
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+} from 'reducers/application';
 
 const initialState = {
   day: 'Monday',
   days: [],
   appointments: {},
   interviewers: {},
-};
-
-const reducerLookup = {
-  [SET_DAY]: (state, action) => ({ ...state, day: action.value }),
-  [SET_APPLICATION_DATA]: (state, action) => ({ ...state, ...action.value }),
-  [SET_INTERVIEW]: (state, action) => {
-    const appointment = {
-      ...state.appointments[action.id],
-      interview: action.interview,
-    };
-    const appointments = {
-      ...state.appointments,
-      [action.id]: appointment,
-    };
-
-    const newState = { ...state, appointments };
-
-    const [interviewDayData] = [...newState.days].filter(day =>
-      day.appointments.includes(action.id)
-    );
-
-    const newSpots = interviewDayData.appointments.reduce(
-      (spots, appointmentId) => {
-        spots -= newState.appointments[appointmentId].interview ? 1 : 0;
-        return spots;
-      },
-      interviewDayData.appointments.length
-    );
-
-    const days = [...state.days].map(day => {
-      if (interviewDayData.id === day.id) {
-        return { ...interviewDayData, spots: newSpots };
-      } else {
-        return day;
-      }
-    });
-
-    return { ...newState, days };
-  },
-  default: action => {
-    throw new Error(
-      `Tried to reduce with unsupported action type: ${action.type}`
-    );
-  },
-};
-
-const reducer = (state, action) => {
-  return (
-    (reducerLookup[action.type] && reducerLookup[action.type](state, action)) ||
-    reducerLookup.default(state, action)
-  );
 };
 
 const useApplication = () => {
---
+++ src/reducers/application.js
@@ -0,0 +1,58 @@
+const SET_DAY = 'SET_DAY';
+const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
+const SET_INTERVIEW = 'SET_INTERVIEW';
+
+const reducerLookup = {
+  [SET_DAY]: (state, action) => ({ ...state, day: action.value }),
+  [SET_APPLICATION_DATA]: (state, action) => ({ ...state, ...action.value }),
+  [SET_INTERVIEW]: (state, action) => {
+    const appointment = {
+      ...state.appointments[action.id],
+      interview: action.interview,
+    };
+    const appointments = {
+      ...state.appointments,
+      [action.id]: appointment,
+    };
+
+    const newState = { ...state, appointments };
+
+    const [interviewDayData] = [...newState.days].filter(day =>
+      day.appointments.includes(action.id)
+    );
+
+    const newSpots = interviewDayData.appointments.reduce(
+      (spots, appointmentId) => {
+        spots -= newState.appointments[appointmentId].interview ? 1 : 0;
+        return spots;
+      },
+      interviewDayData.appointments.length
+    );
+
+    const days = [...state.days].map(day => {
+      if (interviewDayData.id === day.id) {
+        return { ...interviewDayData, spots: newSpots };
+      } else {
+        return day;
+      }
+    });
+
+    return { ...newState, days };
+  },
+  default: action => {
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    );
+  },
+};
+
+const reducer = (state, action) => {
+  return (
+    (reducerLookup[action.type] && reducerLookup[action.type](state, action)) ||
+    reducerLookup.default(action)
+  );
+};
+
+export default reducer;
+
+export { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW };
