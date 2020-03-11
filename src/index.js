b5d9c53f7b3c0d5e5f90e20fa67b2ec5d374b65e
fgfl <frederick.gfl@gmail.com>
2020-03-10 17:20:59 -0700

    console.log env

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import Application from 'components/Application';
 
 import axios from 'axios';
 
+console.log('base url:', process.env);
 if (process.env.REACT_APP_API_BASE_URL) {
+  console.log(process.env.REACT_APP_API_BASE_URL);
   axios.defaults.baseURL = process.REACT_APP_API_BASE_URL;
 }
 
