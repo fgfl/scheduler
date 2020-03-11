608e698a4fd946a77004c018bdbe694166b48b5e
fgfl <frederick.gfl@gmail.com>
2020-03-10 18:54:08 -0700

    Fix axios not getting correct env variable

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import Application from 'components/Application';
 
 import axios from 'axios';
 
-console.log('base url:', process.env);
 if (process.env.REACT_APP_API_BASE_URL) {
-  console.log(process.env.REACT_APP_API_BASE_URL);
-  axios.defaults.baseURL = process.REACT_APP_API_BASE_URL;
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
 
 ReactDOM.render(<Application />, document.getElementById('root'));
